b1d84cf62c2a9b8a1905f4fb2c50516f45b5f8be
farhan0167 <afi225@nyu.edu>
2023-03-05 16:18:04 -0500

    third commit

--- app.py
+++ app.py
@@ -8,3 +8,17 @@ num = 5
 fact = factorial(num)
 
 print(f"The factorial of {num} is {fact}")
+
+def change_cur_dir(self,path):
+        """
+        Given a directory, cd in the directory
+        return the directory path
+        """
+        if path == self.root:
+            os.chdir(path)
+            self.cur_dir = path
+            return path
+        forward_path = self.cur_dir+'/'+path
+        os.chdir(forward_path)
+        self.cur_dir = forward_path
+        return forward_path
